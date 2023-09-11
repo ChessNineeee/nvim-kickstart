ade441dced6bf12d34e25f8d9997375888915933
zhengqi <709030194@qq.com>
2023-09-11 15:24:07 +0800

    add: go extension

--- init.lua
+++ init.lua
@@ -113,6 +113,21 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -308,6 +323,14 @@ require("lazy").setup({
   -- { import = 'custom.plugins' },
 }, {})
 
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    require('go.format').gofmt()
+  end,
+  group = format_sync_grp,
+})
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
